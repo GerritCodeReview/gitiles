1ad96efde9611308896c6f1ad2280fda13d1debc
Dave Borowitz <dborowitz@google.com>
2013-01-11 12:23:22 -0800

    Use the SLF4J simple logger binding for the dev server
    Production servers may want to use log4j12 or similar, but the point of
    the dev server is to avoid the need for additional configuration.
    
    Change-Id: I4bb3f37b9802b1e4d02fe9305162b43af52879c4

--- gitiles-dev/src/main/java/com/google/gitiles/dev/DevServer.java
+++ gitiles-dev/src/main/java/com/google/gitiles/dev/DevServer.java
@@ -138,9 +138,7 @@ class DevServer {
       fcfg.load();
       cfg = fcfg;
     } else {
-      // TODO(dborowitz): This is not getting outputted, we're probably missing
-      // some logging config.
-      log.info("Config file %s not found, using defaults", cfgFile.getPath());
+      log.info("Config file {} not found, using defaults", cfgFile.getPath());
     }
     this.cfg = cfg;
 
