60af12c91271506a5b39372a880f99e1da0d5d42
Dave Borowitz <dborowitz@google.com>
2014-03-16 14:16:15 -0700

    Support JSON view of commits in RevisionServlet
    Example output:
    
    $ curl 'http://localhost:8080/gitiles/+/1040ab8d5861e2804f725a936ca9b6f3be6f6239?format=JSON'
    )]}'
    {
      "commit": "1040ab8d5861e2804f725a936ca9b6f3be6f6239",
      "parents": [
        "0ad024fb5aa3e4994ede801428b4e30ba06a837d"
      ],
      "author": {
        "name": "Dave Borowitz",
        "email": "dborowitz@google.com",
        "time": "Sun Mar 16 13:49:37 2014 -0700"
      },
      "committer": {
        "name": "Dave Borowitz",
        "email": "dborowitz@google.com",
        "time": "Sun Mar 16 13:49:37 2014 -0700"
      },
      "message": "Use CommitData to produce CommitJsonData\n\nChange-Id: I7261b0c40e97119c32627c4ea89038bbe174925f\n",
      "tree_diff": [
        {
          "type": "modify",
          "old_id": "87f3456c40f24662d28411fda9b2cf05b82bf007",
          "old_mode": 33188,
          "old_path": "gitiles-servlet/src/main/java/com/google/gitiles/CommitJsonData.java",
          "new_id": "07fbd1e0eb753aa41dee53c758ced595f18f67bf",
          "new_mode": 33188,
          "new_path": "gitiles-servlet/src/main/java/com/google/gitiles/CommitJsonData.java"
        },
        {
          "type": "modify",
          "old_id": "aa6bd34861cf2f49abd357edff0b2f3ff354aa91",
          "old_mode": 33188,
          "old_path": "gitiles-servlet/src/main/java/com/google/gitiles/LogServlet.java",
          "new_id": "e6ae4c2062bfe3dfb6f059f282e90af64bbd10d3",
          "new_mode": 33188,
          "new_path": "gitiles-servlet/src/main/java/com/google/gitiles/LogServlet.java"
        }
      ]
    }
    
    Change-Id: I29b76ce331bd6876476e3ddd56f2f484450b7e87

--- gitiles-servlet/src/main/java/com/google/gitiles/CommitJsonData.java
+++ gitiles-servlet/src/main/java/com/google/gitiles/CommitJsonData.java
@@ -21,6 +21,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.gitiles.CommitData.Field;
 
+import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
@@ -28,11 +29,12 @@ import org.eclipse.jgit.util.GitDateFormatter;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
 class CommitJsonData {
-  private static final ImmutableSet<Field> DEFAULT_FIELDS = Sets.immutableEnumSet(
+  static final ImmutableSet<Field> DEFAULT_FIELDS = Sets.immutableEnumSet(
       Field.SHA, Field.PARENTS, Field.AUTHOR, Field.COMMITTER, Field.MESSAGE);
 
   static class Commit {
@@ -41,12 +43,25 @@ class CommitJsonData {
     Ident author;
     Ident committer;
     String message;
+
+    List<Diff> treeDiff;
   }
 
   static class Ident {
     String name;
     String email;
     String time;
+  }
+
+  static class Diff {
+    String type;
+    String oldId;
+    int oldMode;
+    String oldPath;
+    String newId;
+    int newMode;
+    String newPath;
+    Integer score;
   }
 
   private RevWalk walk;
@@ -56,13 +71,17 @@ class CommitJsonData {
     return this;
   }
 
-  Commit toJsonData(HttpServletRequest req, RevCommit c, GitDateFormatter df)
+  Commit toJsonData(HttpServletRequest req, RevCommit c, GitDateFormatter df) throws IOException {
+    return toJsonData(req, c, DEFAULT_FIELDS, df);
+  }
+
+  Commit toJsonData(HttpServletRequest req, RevCommit c, Set<Field> fs, GitDateFormatter df)
       throws IOException {
     CommitData.Builder cdb = new CommitData.Builder();
     if (walk != null) {
       cdb.setRevWalk(walk);
     }
-    CommitData cd = cdb.build(req, c, DEFAULT_FIELDS);
+    CommitData cd = cdb.build(req, c, fs);
 
     Commit result = new Commit();
     if (cd.sha != null) {
@@ -83,6 +102,9 @@ class CommitJsonData {
     if (cd.message != null) {
       result.message = cd.message;
     }
+    if (cd.diffEntries != null) {
+      result.treeDiff = toJsonData(cd.diffEntries);
+    }
     return result;
   }
 
@@ -91,6 +113,32 @@ class CommitJsonData {
     result.name = ident.getName();
     result.email = ident.getEmailAddress();
     result.time = df.formatDate(ident);
+    return result;
+  }
+
+  private static List<Diff> toJsonData(List<DiffEntry> diffEntries) {
+    List<Diff> result = Lists.newArrayListWithCapacity(diffEntries.size());
+    for (DiffEntry de : diffEntries) {
+      Diff d = new Diff();
+      d.type = de.getChangeType().name().toLowerCase();
+      d.oldId = de.getOldId().name();
+      d.oldMode = de.getOldMode().getBits();
+      d.oldPath = de.getOldPath();
+      d.newId = de.getNewId().name();
+      d.newMode = de.getNewMode().getBits();
+      d.newPath = de.getNewPath();
+
+      switch (de.getChangeType()) {
+        case COPY:
+        case RENAME:
+          d.score = de.getScore();
+          break;
+        default:
+          break;
+      }
+
+      result.add(d);
+    }
     return result;
   }
 }
