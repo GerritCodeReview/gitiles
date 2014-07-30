b4c78ba9d47c47c165a9a35415c7b61210956cc4
Dave Borowitz <dborowitz@google.com>
2014-07-30 12:45:09 -0700

    Move RepositoryIndexServletTest methods to RefServletTest
    These are just testing the output of static methods in RefServlet.
    There are no repository-index-specific tests remaining in this class,
    so we can delete it entirely.
    
    Change-Id: I0d9ff2c42ca7ca26d37d30364782f0dc29f27656

--- gitiles-servlet/src/test/java/com/google/gitiles/RefServletTest.java
+++ gitiles-servlet/src/test/java/com/google/gitiles/RefServletTest.java
@@ -14,8 +14,12 @@
 
 package com.google.gitiles;
 
+import static com.google.gitiles.TestGitilesUrls.URLS;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
 import org.eclipse.jgit.internal.storage.dfs.DfsRepository;
 import org.eclipse.jgit.internal.storage.dfs.DfsRepositoryDescription;
@@ -25,10 +29,15 @@ import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevTag;
+import org.eclipse.jgit.revwalk.RevWalk;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 /** Tests for {@link Linkifier}. */
 public class RefServletTest {
@@ -39,28 +48,21 @@ public class RefServletTest {
   public void setUp() throws Exception {
     DfsRepository r = new InMemoryRepository(new DfsRepositoryDescription("test"));
     repo = new TestRepository<DfsRepository>(r);
+    servlet = TestGitilesServlet.create(repo);
+  }
 
+  private void setUpSimpleRefs() throws Exception {
     RevCommit commit = repo.branch("refs/heads/master").commit().create();
     repo.update("refs/heads/branch", commit);
     repo.update("refs/tags/ctag", commit);
     RevTag tag = repo.tag("atag", commit);
     repo.update("refs/tags/atag", tag);
-    r.updateRef("HEAD").link("refs/heads/master");
-
-    servlet = TestGitilesServlet.create(repo);
-  }
-
-  private String id(String refName) throws IOException {
-    return ObjectId.toString(repo.getRepository().getRef(refName).getObjectId());
-  }
-
-  private String peeled(String refName) throws IOException {
-    return ObjectId.toString(repo.getRepository().peel(
-          repo.getRepository().getRef(refName)).getPeeledObjectId());
+    repo.getRepository().updateRef("HEAD").link("refs/heads/master");
   }
 
   @Test
   public void evilRefName() throws Exception {
+    setUpSimpleRefs();
     String evilRefName = "refs/evil/<script>window.close();</script>/&foo";
     assertTrue(Repository.isValidRefName(evilRefName));
     repo.branch(evilRefName).commit().create();
@@ -78,6 +80,7 @@ public class RefServletTest {
 
   @Test
   public void getRefsTextAll() throws Exception {
+    setUpSimpleRefs();
     FakeHttpServletRequest req = FakeHttpServletRequest.newRequest();
     req.setPathInfo("/test/+refs");
     req.setQueryString("format=TEXT");
@@ -97,6 +100,7 @@ public class RefServletTest {
 
   @Test
   public void getRefsTextAllTrailingSlash() throws Exception {
+    setUpSimpleRefs();
     FakeHttpServletRequest req = FakeHttpServletRequest.newRequest();
     req.setPathInfo("/test/+refs");
     req.setQueryString("format=TEXT");
@@ -116,6 +120,7 @@ public class RefServletTest {
 
   @Test
   public void getRefsHeadsText() throws Exception {
+    setUpSimpleRefs();
     FakeHttpServletRequest req = FakeHttpServletRequest.newRequest();
     req.setPathInfo("/test/+refs/heads");
     req.setQueryString("format=TEXT");
@@ -131,6 +136,7 @@ public class RefServletTest {
 
   @Test
   public void getRefsHeadsTextTrailingSlash() throws Exception {
+    setUpSimpleRefs();
     FakeHttpServletRequest req = FakeHttpServletRequest.newRequest();
     req.setPathInfo("/test/+refs/heads/");
     req.setQueryString("format=TEXT");
@@ -146,6 +152,7 @@ public class RefServletTest {
 
   @Test
   public void noHeadText() throws Exception {
+    setUpSimpleRefs();
     FakeHttpServletRequest req = FakeHttpServletRequest.newRequest();
     req.setPathInfo("/test/+refs/HEAD");
     req.setQueryString("format=TEXT");
@@ -159,6 +166,7 @@ public class RefServletTest {
 
   @Test
   public void singleHeadText() throws Exception {
+    setUpSimpleRefs();
     FakeHttpServletRequest req = FakeHttpServletRequest.newRequest();
     req.setPathInfo("/test/+refs/heads/master");
     req.setQueryString("format=TEXT");
@@ -173,6 +181,7 @@ public class RefServletTest {
 
   @Test
   public void singlePeeledTagText() throws Exception {
+    setUpSimpleRefs();
     FakeHttpServletRequest req = FakeHttpServletRequest.newRequest();
     req.setPathInfo("/test/+refs/tags/atag");
     req.setQueryString("format=TEXT");
@@ -184,5 +193,113 @@ public class RefServletTest {
         id("refs/tags/atag") + " refs/tags/atag\n"
         + peeled("refs/tags/atag") + " refs/tags/atag^{}\n",
         res.getActualBodyString());
+  }
+
+  @Test
+  public void emptySoy() throws Exception {
+    assertEquals(ImmutableList.of(), buildBranchesSoyData());
+    assertEquals(ImmutableList.of(), buildTagsSoyData());
+  }
+
+  @Test
+  public void branchesAndTagsSoy() throws Exception {
+    repo.branch("refs/heads/foo").commit().create();
+    repo.branch("refs/heads/bar").commit().create();
+    repo.branch("refs/tags/baz").commit().create();
+    repo.branch("refs/nope/quux").commit().create();
+
+    assertEquals(
+        ImmutableList.of(
+            ref("/b/test/+/bar", "bar"),
+            ref("/b/test/+/foo", "foo")),
+        buildBranchesSoyData());
+    assertEquals(
+        ImmutableList.of(
+            ref("/b/test/+/baz", "baz")),
+        buildTagsSoyData());
+  }
+
+  @Test
+  public void ambiguousBranchSoy() throws Exception {
+    repo.branch("refs/heads/foo").commit().create();
+    repo.branch("refs/heads/bar").commit().create();
+    repo.branch("refs/tags/foo").commit().create();
+
+    assertEquals(
+        ImmutableList.of(
+            ref("/b/test/+/bar", "bar"),
+            ref("/b/test/+/refs/heads/foo", "foo")),
+        buildBranchesSoyData());
+    assertEquals(
+        ImmutableList.of(
+            // refs/tags/ is searched before refs/heads/, so this does not
+            // appear ambiguous.
+            ref("/b/test/+/foo", "foo")),
+        buildTagsSoyData());
+  }
+
+  @Test
+  public void ambiguousRelativeToNonBranchOrTagSoy() throws Exception {
+    repo.branch("refs/foo").commit().create();
+    repo.branch("refs/heads/foo").commit().create();
+    repo.branch("refs/tags/foo").commit().create();
+
+    assertEquals(
+        ImmutableList.of(
+            ref("/b/test/+/refs/heads/foo", "foo")),
+        buildBranchesSoyData());
+    assertEquals(
+        ImmutableList.of(
+            ref("/b/test/+/refs/tags/foo", "foo")),
+        buildTagsSoyData());
+  }
+
+  @Test
+  public void refsHeadsSoy() throws Exception {
+    repo.branch("refs/heads/foo").commit().create();
+    repo.branch("refs/heads/refs/heads/foo").commit().create();
+
+    assertEquals(
+        ImmutableList.of(
+            ref("/b/test/+/foo", "foo"),
+            ref("/b/test/+/refs/heads/refs/heads/foo", "refs/heads/foo")),
+        buildBranchesSoyData());
+  }
+
+  private HttpServletRequest buildSoyRequest() {
+    HttpServletRequest req = FakeHttpServletRequest.newRequest(repo.getRepository());
+    ViewFilter.setView(req, GitilesView.repositoryIndex()
+        .setHostName(URLS.getHostName(req))
+        .setServletPath(req.getServletPath())
+        .setRepositoryName("test")
+        .build());
+    return req;
+  }
+
+  private List<?> buildBranchesSoyData() throws Exception {
+    return RefServlet.getBranchesSoyData(buildSoyRequest(), Integer.MAX_VALUE);
+  }
+
+  private List<?> buildTagsSoyData() throws Exception {
+    RevWalk rw = new RevWalk(repo.getRepository());
+    try {
+      return RefServlet.getTagsSoyData(buildSoyRequest(),
+          new TimeCache(TimeCache.defaultBuilder()), rw, Integer.MAX_VALUE);
+    } finally {
+      rw.release();
+    }
+  }
+
+  private String id(String refName) throws IOException {
+    return ObjectId.toString(repo.getRepository().getRef(refName).getObjectId());
+  }
+
+  private String peeled(String refName) throws IOException {
+    return ObjectId.toString(repo.getRepository().peel(
+          repo.getRepository().getRef(refName)).getPeeledObjectId());
+  }
+
+  private Map<String, String> ref(String url, String name) {
+    return ImmutableMap.of("url", url, "name", name);
   }
 }
