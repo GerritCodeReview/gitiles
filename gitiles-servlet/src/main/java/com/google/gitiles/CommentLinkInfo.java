6863c35c53d5996f5ff36b211502ce89eee8ea0d
Stefan Beller <sbeller@google.com>
2014-11-07 11:29:20 -0800

    Linkify commit messages using regexp-based rules
    This commit adds a new config option to gitiles.
    Similar to Gerrit commentlinks[1], there may be one or more config
    sections "commentlink", which will have a 'match' and link name.
    If the regular expression as specified in 'match' matches, the part of
    the text will be linkified with the 'link' value as a prefix to the link.
    
    Until now there was just one regular expression covering both standard
    web links as well gerrit ChangeId links. We cannot put the configurable
    comment links into that ChangeId as well, because there you can have
    references to groups within the regular expression and resolving the
    references within one large regular expression would get quite messy.
    
    Therefore this commit introduces a new class CommentLinkInfo,
    which will do the actual replacing for one configurable or
    the web links or the change ids.
    
    [1] http://gerrit-documentation.googlecode.com/svn/Documentation/2.5.1/config-gerrit.html#_a_id_commentlink_a_section_commentlink
    
    Change-Id: Ic4d75aa55d648fc492559ff13b1dbebf2a1ff4b5
    Signed-off-by: Stefan Beller <sbeller@google.com>

---
+++ gitiles-servlet/src/main/java/com/google/gitiles/CommentLinkInfo.java
@@ -0,0 +1,95 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gitiles;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.template.soy.data.UnsafeSanitizedContentOrdainer;
+import com.google.template.soy.data.SanitizedContent;
+
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class CommentLinkInfo {
+  private final String link;
+  private final String html;
+  private final String name;
+  private final Pattern pattern;
+
+  public CommentLinkInfo(String name, Pattern compiledMatch, String link, String html) {
+    checkArgument(name != null, "invalid commentlink.name");
+    link = Strings.emptyToNull(link);
+    html = Strings.emptyToNull(html);
+    this.name = name;
+    this.link = link;
+    this.html = html;
+    this.pattern = compiledMatch;
+  }
+
+  public List<Map<String, String>> replace(String input) {
+    List<Map<String, String>> parsed = Lists.newArrayList();
+    Matcher m = pattern.matcher(input);
+    int last = 0;
+    while (m.find()) {
+      addText(parsed, input.substring(last, m.start()));
+      if (link != null) {
+        String builder = new String(link);
+        for (int i = 0; i < m.groupCount(); i++) {
+          builder = builder.replaceAll("\\$" + Integer.toString(i + 1), m.group(i + 1));
+        }
+        addLink(parsed, m.group(0), builder);
+      } else {
+        String builder = new String(html);
+        for (int i = 0; i < m.groupCount(); i++) {
+          builder = builder.replaceAll("\\$" + Integer.toString(i + 1), m.group(i + 1));
+        }
+        addHtml(parsed, builder);
+      }
+      last = m.end();
+    }
+    addText(parsed, input.substring(last));
+    return parsed;
+  }
+
+  private static void addLink(List<Map<String, String>> parts, String text, String url) {
+    parts.add(ImmutableMap.of("text", text, "url", url));
+  }
+
+  private static void addHtml(List<Map<String, String>> parts, String html) {
+    parts.add(ImmutableMap.of("html", UnsafeSanitizedContentOrdainer.ordainAsSafe(html, SanitizedContent.ContentKind.HTML).toString()));
+  }
+
+  private static void addText(List<Map<String, String>> parts, String text) {
+    if (text.isEmpty()) {
+      return;
+    }
+    if (parts.isEmpty()) {
+      parts.add(ImmutableMap.of("text", text));
+    } else {
+      Map<String, String> old = parts.get(parts.size() - 1);
+      if (!old.containsKey("url")) {
+        parts.set(parts.size() - 1, ImmutableMap.of("text", old.get("text") + text));
+      } else {
+        parts.add(ImmutableMap.of("text", text));
+      }
+    }
+  }
+}
