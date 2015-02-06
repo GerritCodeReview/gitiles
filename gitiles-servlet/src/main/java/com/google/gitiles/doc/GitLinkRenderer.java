fe60ba1b4b7449dd36d9c9330f7b58d6621070a6
Shawn Pearce <sop@google.com>
2015-02-06 14:49:33 -0800

    Markdown: render pages under /+doc/
    Parse and render any file ending with ".md" as markdown using the
    pegdown processor.  This allows a repository to be used as a
    documentation source.
    
    If "navbar.md" is found in the top level directory it is rendered as a
    horizontal bar across the top of the page.  A navbar.md is an outline:
    
      * [Home](/index.md)
      * [APIs](/api/index.md)
      * [Source](/src/main/java/index.md)
    
    Any links starting with "/" and ending in ".md" are resolved as
    relative to the top of the Git repository.
    
    As an extension to Markdown the special block [TOC] is replaced with
    a table of contents from the headers of the text.
    
    Markdown support can be optionally disabled by setting the config
    variable markdown.render to false.
    
    Change-Id: Ic111628c59cfadfdca37bf0cc637ee8a14d54c37

---
+++ gitiles-servlet/src/main/java/com/google/gitiles/doc/GitLinkRenderer.java
@@ -0,0 +1,142 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gitiles.doc;
+
+import static org.pegdown.FastEncoder.encode;
+
+import com.google.common.base.Strings;
+import com.google.gitiles.GitilesView;
+import com.google.template.soy.shared.restricted.EscapingConventions;
+import com.google.template.soy.shared.restricted.EscapingConventions.FilterImageDataUri;
+import com.google.template.soy.shared.restricted.EscapingConventions.FilterNormalizeUri;
+
+import org.pegdown.LinkRenderer;
+import org.pegdown.ast.AutoLinkNode;
+import org.pegdown.ast.ExpImageNode;
+import org.pegdown.ast.ExpLinkNode;
+import org.pegdown.ast.RefImageNode;
+import org.pegdown.ast.RefLinkNode;
+import org.pegdown.ast.WikiLinkNode;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+/** Resolves {@code [foo](/path/to.md)} as relative to repository root. */
+class GitLinkRenderer extends LinkRenderer {
+  private static final FilterNormalizeUri URI =
+      EscapingConventions.FilterNormalizeUri.INSTANCE;
+
+  private static final FilterImageDataUri IMAGE_DATA =
+      EscapingConventions.FilterImageDataUri.INSTANCE;
+
+  private final GitilesView view;
+
+  GitLinkRenderer(GitilesView view) {
+    this.view = view;
+  }
+
+  @Override
+  public Rendering render(AutoLinkNode node) {
+    String url = node.getText();
+    return renderLink(url, null, url);
+  }
+
+  @Override
+  public Rendering render(ExpLinkNode node, String text) {
+    return renderLink(node.url, node.title, text);
+  }
+
+  @Override
+  public Rendering render(RefLinkNode node, String url,
+      String title, String text) {
+    return renderLink(url, title, text);
+  }
+
+  @Override
+  public Rendering render(WikiLinkNode node) {
+    try {
+      String path = node.getText().replace(' ', '-') + ".md";
+      String url = URLEncoder.encode(path, StandardCharsets.UTF_8.name());
+      return renderLink(url, null, node.getText());
+    } catch (UnsupportedEncodingException e) {
+      throw new IllegalStateException();
+    }
+  }
+
+  Rendering renderLink(String url, String title, String text) {
+    if (isMarkdownUrl(url)) {
+      url = getMarkdownUrl(url);
+    }
+
+    String href;
+    if (URI.getValueFilter().matcher(url).find()) {
+      href = URI.escape(url);
+    } else {
+      href = URI.getInnocuousOutput();
+    }
+
+    Rendering r = new Rendering(href, text);
+    if (!Strings.isNullOrEmpty(title)) {
+      r = r.withAttribute("title", encode(title));
+    }
+    return r;
+  }
+
+  @Override
+  public Rendering render(ExpImageNode node, String text) {
+    return renderImage(node.url, node.title, text);
+  }
+
+  @Override
+  public Rendering render(RefImageNode node,
+      String url, String title, String alt) {
+    return renderImage(url, title, alt);
+  }
+
+  private Rendering renderImage(String url, String title, String alt) {
+    String src;
+    if ((url.startsWith("http:") || url.startsWith("https:"))
+        && URI.getValueFilter().matcher(url).find()) {
+      src = URI.escape(url);
+    } else if (isImageDataUrl(url)) {
+      src = url;
+    } else {
+      src = IMAGE_DATA.getInnocuousOutput();
+    }
+
+    Rendering r = new Rendering(src, alt);
+    if (!Strings.isNullOrEmpty(title)) {
+      r = r.withAttribute("title", encode(title));
+    }
+    return r;
+  }
+
+  static boolean isImageDataUrl(String url) {
+    return IMAGE_DATA.getValueFilter().matcher(url).find();
+  }
+
+  static boolean isMarkdownUrl(String url) {
+    return url.length() > 2
+        && url.endsWith(".md")
+        && url.charAt(0) == '/' && url.charAt(1) != '/';
+  }
+
+  String getMarkdownUrl(String url) {
+    return GitilesView.doc().copyFrom(view)
+        .setPathPart(url.substring(1))
+        .toUrl();
+  }
+}
---
+++ gitiles-servlet/src/main/java/com/google/gitiles/doc/HtmlFormatter.java
@@ -0,0 +1,61 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gitiles.doc;
+
+import com.google.common.collect.ImmutableList;
+
+import org.pegdown.LinkRenderer;
+import org.pegdown.ToHtmlSerializer;
+import org.pegdown.ast.HeaderNode;
+import org.pegdown.plugins.ToHtmlSerializerPlugin;
+
+class HtmlFormatter extends ToHtmlSerializer {
+  private final TocSerializer toc;
+
+  HtmlFormatter(LinkRenderer linkRenderer, TocSerializer toc) {
+    super(linkRenderer, ImmutableList.<ToHtmlSerializerPlugin> of(toc));
+    this.toc = toc;
+  }
+
+  @Override
+  public void visit(HeaderNode node) {
+    String tag = "h" + node.getLevel();
+    printer.print('<').print(tag);
+    if (toc.include(node)) {
+      String id = toc.idFromHeader(node);
+      if (id != null) {
+        printer.print(" id=\"").print(id).print('"');
+      }
+    }
+    printer.print('>');
+    visitChildren(node);
+    printer.print('<').print('/').print(tag).print('>');
+  }
+
+  @Override
+  protected void printImageTag(LinkRenderer.Rendering rendering) {
+      printer.print("<img");
+      printAttribute("src", rendering.href);
+      printAttribute("alt", rendering.text);
+      for (LinkRenderer.Attribute attr : rendering.attributes) {
+          printAttribute(attr.name, attr.value);
+      }
+      printer.print(" />");
+  }
+
+  private void printAttribute(String n, String v) {
+    printer.print(' ').print(n).print('=').print('"').print(v).print('"');
+  }
+}
