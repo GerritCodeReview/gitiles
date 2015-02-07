8362b2774a0e174d1fab13395026e6ba1bc92322
Shawn Pearce <sop@google.com>
2015-02-07 00:10:24 -0800

    Markdown: recognize special multi-column layout blocks
    Documentation writers may want to organize clusters of information
    and/or links into a multi-column based layout to help readers find
    material quickly.
    
    Extend the supported markdown syntax with a new multi-column region.
    A multi-column region is delineated by |||---||| with other markdown
    contained within it:
    
      |||---|||
      ## Markdown is easy
      You can write simple text.
    
      * [Basic markdown](http://...)
      * [GitHub flavor](http://github.com/...)
      * [This kind](kind.md)
    
      ## Safe
      No HTML found.
    
      ## Fast
      Well, faster than you can get coffee.
      |||---|||
    
    Headers within the region define the columns.  For each header a new
    column is started and the following content is placed below it into
    the same column.
    
    The above will render as three divs next to each other:
    
      Markdown is easy            Safe                 Fast
    
      You can write simple        No HTML found.       Well, faster than
      text.                                            you can get coffee.
    
      * Basic markdown
      * GitHub flavor
      * This kind
    
    At present up to 4 columns may be fit across a page of documentation.
    Additional columns should be placed into a new |||---||| block and
    will appear below these columns.
    
    The column widths are not configurable.
    
    Change-Id: I088115bf4d4569817aca736cb0ce02f9d777d61b

---
+++ gitiles-servlet/src/main/java/com/google/gitiles/doc/html/ColsSerializer.java
@@ -0,0 +1,52 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gitiles.doc.html;
+
+import com.google.gitiles.doc.ColsNode;
+import com.google.gitiles.doc.DivNode;
+
+import org.pegdown.Printer;
+import org.pegdown.ast.HeaderNode;
+import org.pegdown.ast.Node;
+import org.pegdown.ast.Visitor;
+import org.pegdown.plugins.ToHtmlSerializerPlugin;
+
+/** Formats {@code |||---|||} blocks into multi-column {@code &lt;div&gt;}s. */
+class ColsSerializer implements ToHtmlSerializerPlugin {
+  @Override
+  public boolean visit(Node node, Visitor visitor, Printer printer) {
+    if (node instanceof ColsNode) {
+      ColsNode cols = (ColsNode) node;
+      printer.println().print("<div class=\"cols\">").indent(2);
+      boolean open = false;
+      for (Node n : cols.getChildren()) {
+        if (n instanceof HeaderNode || n instanceof DivNode) {
+          if (open) {
+            printer.print("</div>");
+          }
+          printer.println().print("<div class=\"col-3\">");
+          open = true;
+        }
+        n.accept(visitor);
+      }
+      if (open) {
+        printer.print("</div>");
+      }
+      printer.indent(-2).println().print("</div>").println();
+      return true;
+    }
+    return false;
+  }
+}
