9b71814bae12829ac467280285327780477ce05a
Shawn Pearce <sop@google.com>
2015-02-10 15:36:54 -0800

    Markdown: render pages under /+doc/
    Parse and render any file ending with ".md" as markdown using the
    pegdown processor.  This allows a repository to be used as a
    documentation source.
    
    If "navbar.md" is found in the top level directory it is rendered as a
    horizontal bar across the top of the page.  A navbar.md is an outline:
    
      * [Home](/index.md)
      * [APIs](/api/index.md)
      * [Source](/src/main/java/index.md)
    
    Any links starting with "/" and ending in ".md" are resolved as
    relative to the top of the Git repository.
    
    As an extension to Markdown the special block [TOC] is replaced with
    a table of contents from the headers of the text.
    
    Markdown support can be optionally disabled by setting the config
    variable markdown.render to false.
    
    Change-Id: Ic111628c59cfadfdca37bf0cc637ee8a14d54c37

--- gitiles-servlet/src/main/java/com/google/gitiles/doc/MarkdownHelper.java
+++ gitiles-servlet/src/main/java/com/google/gitiles/doc/MarkdownHelper.java
@@ -14,17 +14,71 @@
 
 package com.google.gitiles.doc;
 
-import com.google.common.base.Strings;
+import static org.pegdown.Extensions.ALL;
+import static org.pegdown.Extensions.HARDWRAPS;
+import static org.pegdown.Extensions.SUPPRESS_ALL_HTML;
 
+import com.google.common.base.Strings;
+import com.google.gitiles.GitilesView;
+
+import org.pegdown.ParsingTimeoutException;
+import org.pegdown.PegDownProcessor;
+import org.pegdown.ast.HeaderNode;
 import org.pegdown.ast.Node;
+import org.pegdown.ast.RootNode;
 import org.pegdown.ast.TextNode;
+import org.pegdown.plugins.PegDownPlugins;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MarkdownHelper {
+  private static final Logger log = LoggerFactory.getLogger(MarkdownHelper.class);
+
+  // SUPPRESS_ALL_HTML is enabled to permit hosting arbitrary user content
+  // while avoiding XSS style HTML, CSS and JavaScript injection attacks.
+  //
+  // HARDWRAPS is disabled to permit line wrapping within paragraphs to
+  // make the source file easier to read in 80 column terminals without
+  // this impacting the rendered formatting.
+  static final int MD_OPTIONS = (ALL | SUPPRESS_ALL_HTML) & ~(HARDWRAPS);
+
   /** Check if anchor URL is like {@code /top.md}. */
   public static boolean isAbsolutePathToMarkdown(String url) {
     return url.length() >= 5
         && url.charAt(0) == '/' && url.charAt(1) != '/'
         && url.endsWith(".md");
+  }
+
+  static RootNode parseMarkdown(String markdownSource) {
+    PegDownPlugins plugins = new PegDownPlugins.Builder()
+        .withPlugin(GitilesMarkdown.class)
+        .build();
+    return new PegDownProcessor(MD_OPTIONS, plugins)
+        .parseMarkdown(markdownSource.toCharArray());
+  }
+
+  static RootNode parseFile(GitilesView view, String path, String md) {
+    if (md == null) {
+      return null;
+    }
+
+    RootNode docTree;
+    try {
+      docTree = MarkdownHelper.parseMarkdown(md);
+    } catch (ParsingTimeoutException e) {
+      log.error("timeout rendering {}/{} at {}",
+          view.getRepositoryName(),
+          path,
+          view.getRevision().getName());
+      return null;
+    }
+    if (docTree == null) {
+      log.error("cannot parse {}/{} at {}",
+          view.getRepositoryName(),
+          path,
+          view.getRevision().getName());
+    }
+    return docTree;
   }
 
   /** Combine child nodes as string; this must be escaped for HTML. */
@@ -46,6 +100,23 @@ public class MarkdownHelper {
         appendTextFromChildren(b, child);
       }
     }
+  }
+
+  static String getTitle(Node node) {
+    if (node instanceof HeaderNode) {
+      if (((HeaderNode) node).getLevel() == 1) {
+        return getInnerText(node);
+      }
+      return null;
+    }
+
+    for (Node child : node.getChildren()) {
+      String title = getTitle(child);
+      if (title != null) {
+        return title;
+      }
+    }
+    return null;
   }
 
   private MarkdownHelper() {
