c24d25c1eb4dcd4e007f3a33c3caedf01b021cac
Shawn Pearce <sop@google.com>
2015-02-11 01:11:25 -0800

    foo
    Change-Id: I2dfb4c52d781251e9e4274d82760c6695eab76ac

--- gitiles-servlet/src/main/java/com/google/gitiles/doc/Visitor.java
+++ gitiles-servlet/src/main/java/com/google/gitiles/doc/Visitor.java
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 package com.google.gitiles.doc;
 
 public interface Visitor extends org.pegdown.ast.Visitor {
