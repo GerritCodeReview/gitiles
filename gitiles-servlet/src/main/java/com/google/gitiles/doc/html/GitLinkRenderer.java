72d9ceaf51969a14c555d32d109c8793dcdb32a0
Shawn Pearce <sop@google.com>
2015-02-10 16:23:46 -0800

    Markdown: add minimal support for AST to HTML
    Includes extension support to walk the AST and produce a table
    of contents from the contained HeaderNodes.
    
    Rendered HTML is blessed as SanitizedContent for Soy to include
    inside of templates without further escaping.
    
    Change-Id: Ib53e091617b39e22a230da9ec8a4aa39c3bf5c26

---
+++ gitiles-servlet/src/main/java/com/google/gitiles/doc/html/GitLinkRenderer.java
@@ -0,0 +1,121 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gitiles.doc.html;
+
+import static org.pegdown.FastEncoder.encode;
+
+import com.google.common.base.Strings;
+import com.google.template.soy.shared.restricted.EscapingConventions;
+import com.google.template.soy.shared.restricted.EscapingConventions.FilterImageDataUri;
+import com.google.template.soy.shared.restricted.EscapingConventions.FilterNormalizeUri;
+
+import org.pegdown.LinkRenderer;
+import org.pegdown.ast.AutoLinkNode;
+import org.pegdown.ast.ExpImageNode;
+import org.pegdown.ast.ExpLinkNode;
+import org.pegdown.ast.RefImageNode;
+import org.pegdown.ast.RefLinkNode;
+import org.pegdown.ast.WikiLinkNode;
+
+/** Sanitizes URLs by applying Soy's URI functions to them. */
+class GitLinkRenderer extends LinkRenderer {
+  private static final FilterNormalizeUri URI =
+      EscapingConventions.FilterNormalizeUri.INSTANCE;
+
+  private static final FilterImageDataUri IMAGE_DATA =
+      EscapingConventions.FilterImageDataUri.INSTANCE;
+
+  @Override
+  public Rendering render(ExpLinkNode node, String textEscaped) {
+    return renderLink(node.url, node.title, textEscaped);
+  }
+
+  @Override
+  public Rendering render(RefLinkNode node, String url,
+      String title, String textEscaped) {
+    return renderLink(url, title, textEscaped);
+  }
+
+  @Override
+  public Rendering render(AutoLinkNode node) {
+    String url = node.getText();
+    return renderLink(url, null, encode(url));
+  }
+
+  @Override
+  public Rendering render(WikiLinkNode node) {
+    String text = node.getText();
+    String url = text.replace(' ', '-') + ".md";
+    return renderLink(url, null, encode(text));
+  }
+
+  private Rendering renderLink(String url, String title, String textEscaped) {
+    if (isAbsoluteMarkdown(url)) {
+      url = "#zSoyTodoz";
+    }
+
+    String href;
+    if (URI.getValueFilter().matcher(url).find()) {
+      href = URI.escape(url);
+    } else {
+      href = URI.getInnocuousOutput();
+    }
+
+    Rendering r = new Rendering(href, textEscaped);
+    if (!Strings.isNullOrEmpty(title)) {
+      r = r.withAttribute("title", encode(title));
+    }
+    return r;
+  }
+
+  @Override
+  public Rendering render(ExpImageNode node, String altEscaped) {
+    return renderImage(node.url, node.title, altEscaped);
+  }
+
+  @Override
+  public Rendering render(RefImageNode node,
+      String url, String title, String altEscaped) {
+    return renderImage(url, title, altEscaped);
+  }
+
+  private Rendering renderImage(String url, String title, String altEscaped) {
+    String src;
+    if ((url.startsWith("http:") || url.startsWith("https:"))
+        && URI.getValueFilter().matcher(url).find()) {
+      src = URI.escape(url);
+    } else if (isImageDataUrl(url)) {
+      src = url;
+    } else {
+      src = IMAGE_DATA.getInnocuousOutput();
+    }
+
+    Rendering r = new Rendering(src, altEscaped);
+    if (!Strings.isNullOrEmpty(title)) {
+      r = r.withAttribute("title", encode(title));
+    }
+    return r;
+  }
+
+  public static boolean isImageDataUrl(String url) {
+    return IMAGE_DATA.getValueFilter().matcher(url).find();
+  }
+
+  public static boolean isAbsoluteMarkdown(String url) {
+    return url.length() > 2
+        && url.endsWith(".md")
+        && url.charAt(0) == '/' && url.charAt(1) != '/';
+  }
+}
---
+++ gitiles-servlet/src/main/java/com/google/gitiles/doc/html/TocSerializer.java
@@ -0,0 +1,172 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gitiles.doc.html;
+
+import com.google.gitiles.doc.MarkdownHelper;
+import com.google.gitiles.doc.TocNode;
+
+import org.apache.commons.lang3.StringUtils;
+import org.pegdown.Printer;
+import org.pegdown.ast.HeaderNode;
+import org.pegdown.ast.Node;
+import org.pegdown.ast.RootNode;
+import org.pegdown.ast.Visitor;
+import org.pegdown.plugins.ToHtmlSerializerPlugin;
+
+import java.util.List;
+
+/** Formats table of contents into HTML at {@code [TOC]} block. */
+class TocSerializer implements ToHtmlSerializerPlugin {
+  private final int maxLevel = 3;
+
+  private RootNode root;
+  private Boolean includeH1;
+
+  private int level;
+
+  void setRoot(RootNode doc) {
+    root = doc;
+    includeH1 = null;
+  }
+
+  boolean getIncludeH1() {
+    if (includeH1 == null) {
+      includeH1 = countH1(root) > 1;
+    }
+    return includeH1;
+  }
+
+  boolean include(HeaderNode h) {
+    if (h.getLevel() == 1) {
+      return getIncludeH1();
+    }
+    return h.getLevel() <= maxLevel;
+  }
+
+  @Override
+  public boolean visit(Node node, Visitor visitor, Printer sb) {
+    if (node instanceof TocNode) {
+      int startLevel = getIncludeH1() ? 1 : 2;
+      level = startLevel;
+
+      sb.println();
+      sb.print("<div class=\"toc\" role=\"navigation\">");
+      sb.print("<h2>Contents</h2>");
+      sb.print("<div class=\"toc-aux\">");
+      sb.print("<ul>").indent(2);
+      outline(root, sb);
+      while (level >= startLevel) {
+        sb.indent(-2).println().print("</ul>");
+        level--;
+      }
+      sb.print("</div></div>").println();
+      return true;
+    }
+    return false;
+  }
+
+  private void outline(Node node, Printer sb) {
+    if (node instanceof HeaderNode) {
+      outline((HeaderNode) node, sb);
+    } else if (node.getChildren() != null) {
+      for (Node n : node.getChildren()) {
+        outline(n, sb);
+      }
+    }
+  }
+
+  private void outline(HeaderNode h, Printer sb) {
+    if (!include(h)) {
+      return;
+    }
+
+    String title = MarkdownHelper.getInnerText(h);
+    if (title == null) {
+      return;
+    }
+
+    while (level > h.getLevel()) {
+      sb.indent(-2).println().print("</ul>");
+      level--;
+    }
+    while (level < h.getLevel()) {
+      sb.println().print("<ul>").indent(2);
+      level++;
+    }
+
+    String id = idFromHeader(h);
+    sb.println();
+    sb.print("<li><a href=\"#").printEncoded(id).print("\">");
+    sb.printEncoded(title);
+    sb.print("</a></li>");
+    return;
+  }
+
+  String idFromHeader(HeaderNode header) {
+    String title = MarkdownHelper.getInnerText(header);
+    if (title == null) {
+      return null;
+    }
+
+    StringBuilder b = new StringBuilder(title.length());
+    for (char c : StringUtils.stripAccents(title).toCharArray()) {
+      if (('a' <= c && c <= 'z')
+          || ('A' <= c && c <= 'Z')
+          || ('0' <= c && c <= '9')) {
+        b.append(c);
+      } else if (c == ' ') {
+        if (b.length() > 0
+            && b.charAt(b.length() - 1) != '-'
+            && b.charAt(b.length() - 1) != '_') {
+          b.append('-');
+        }
+      } else if (b.length() > 0
+          && b.charAt(b.length() - 1) != '-'
+          && b.charAt(b.length() - 1) != '_') {
+        b.append('_');
+      }
+    }
+    while (b.length() > 0) {
+      char c = b.charAt(b.length() - 1);
+      if (c == '-' || c == '_') {
+        b.setLength(b.length() - 1);
+        continue;
+      }
+      break;
+    }
+    return b.toString();
+  }
+
+  private static int countH1(Node node) {
+    if (node instanceof HeaderNode) {
+      HeaderNode h = (HeaderNode) node;
+      return h.getLevel() == 1 ? 1 : 0;
+    }
+
+    List<Node> ch = node.getChildren();
+    if (ch == null) {
+      return 0;
+    }
+
+    int count = 0;
+    for (Node n : ch) {
+      count += countH1(n);
+      if (count >= 2) {
+        break;
+      }
+    }
+    return count;
+  }
+}
