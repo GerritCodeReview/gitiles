4d81e2446abd3f6b43dd5ef4aef7e46ca0afbbf1
Dave Borowitz <dborowitz@google.com>
2015-10-26 11:05:07 -0400

    LogServletTest: Use TypeToken to simplify JSON construction
    Change-Id: I1c6822b7261e0348393a215bb38b0d10c3882bae

--- gitiles-servlet/src/test/java/com/google/gitiles/ServletTest.java
+++ gitiles-servlet/src/test/java/com/google/gitiles/ServletTest.java
@@ -20,9 +20,9 @@ import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
 import static javax.servlet.http.HttpServletResponse.SC_OK;
 
 import com.google.common.net.HttpHeaders;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import org.eclipse.jgit.internal.storage.dfs.DfsRepository;
 import org.eclipse.jgit.internal.storage.dfs.DfsRepositoryDescription;
@@ -98,19 +98,33 @@ public class ServletTest {
     String body = res.getActualBodyString();
     String magic = ")]}'\n";
     assertThat(body).startsWith(magic);
-    return body.substring(magic.length());
+    String result = body.substring(magic.length());
+    System.out.println(result);
+    return result;
   }
 
-  protected JsonElement buildJson(String path, String additionalQueryString) throws Exception {
-    return new JsonParser().parse(buildJsonRaw(path, additionalQueryString));
+  protected <T> T buildJson(String path, Class<T> classOfT, String additionalQueryString)
+      throws Exception {
+    return newGson().fromJson(buildJsonRaw(path, additionalQueryString), classOfT);
   }
 
   protected <T> T buildJson(String path, Class<T> classOfT) throws Exception {
-    return new Gson().fromJson(buildJsonRaw(path, ""), classOfT);
+    return buildJson(path, classOfT, "");
+  }
+
+  protected <T> T buildJson(String path, Type typeOfT, String additionalQueryString)
+      throws Exception {
+    return newGson().fromJson(buildJsonRaw(path, additionalQueryString), typeOfT);
   }
 
   protected <T> T buildJson(String path, Type typeOfT) throws Exception {
-    return new Gson().<T>fromJson(buildJsonRaw(path, ""), typeOfT);
+    return buildJson(path, typeOfT, "");
+  }
+
+  private static Gson newGson() {
+    return new GsonBuilder()
+        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+        .create();
   }
 
   protected void assertNotFound(String path, String queryString) throws Exception {
