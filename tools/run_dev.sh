8379bf49b8b3e40eb5fc1c7ff0756df09db5ab65
Han-Wen Nienhuys <hanwen@google.com>
2016-03-30 13:22:27 +0200

    gitiles-dev: inject source path through a System property.
    This makes gitiles-dev independent of the build system. It assumes
    that nobody relies on .buckconfig being shipped into the gitiles jar.
    
    Change-Id: Id3c5313061516aba835f0bc62160133913c7772e

--- tools/run_dev.sh
+++ tools/run_dev.sh
@@ -22,6 +22,8 @@ if [ "x$1" != "x" ]; then
   PROPERTIES="-Dcom.google.gitiles.configPath=$1"
 fi
 
+PROPERTIES="$PROPERTIES -Dcom.google.gitiles.sourcePath=$ROOT"
+
 (
   cd "$ROOT"
   buck build gitiles-dev:dev
