dc84fef4035cb3a955fcc39cdb07f26fe7c182b7
Michael Ochmann <michael.ochmann@sap.com>
2016-09-20 13:20:21 +0200

    WIP: Code Search UI
    This patch aims at utilizing a zoekt server [1] as backend for
    a code search in gitiles. A new view is added, /+search, that
    displays the result of a code search in a given branch of a
    repository for which the zoekt server can provide index data.
    
    zoekt needs the name of a repository and at least one branch name
    as input. Branch names are not easily available in gitiles, though.
    Actually, only the repository index page calculates them, but the
    other views treat branches just as any other revision, which are
    not usable as input for zoekt yet. Therefore, the search form is
    visible only on the repository index page, although for layout
    reasons it had to be added to Common.soy. There is no possibility
    to select multiple (or all) branches for a search query yet in the
    UI to keep things simple.
    
    The search form submits the query and branch to the search as:
    
       /<repo>/+search?branch=<branch>&query=<query>
    
    The search view connects to the zoekt server, which has been
    configured in gitiles.conf, section [codeSearch], for example:
    
    [codeSearch]
      zoektServer = http://zoekt.example.org:6070
    
    TODOs:
      - support searching multiple branches ("all branches" option in
        the dropdown list)
      - make the search available on views, where we can determine a
        branch from the view context
      - extract the HTTP communication code and improve error handling
      - css
    
    [1] https://github.com/google/zoekt
    
    Also-By: Saša Živkov <sasa.zivkov@sap.com>
    Change-Id: I1a01de9ab60baa817da5acf398ca2b5201ccd342

---
+++ gitiles-servlet/src/main/java/com/google/gitiles/search/MatchingFile.java
@@ -0,0 +1,57 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gitiles.search;
+
+import com.google.template.soy.data.SoyListData;
+import com.google.template.soy.data.SoyMapData;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SuppressWarnings("unused")
+class MatchingFile {
+
+  private String repo;
+  private String[] branches;
+  private String fileName;
+  private MatchingLine[] lines;
+
+  static List<Map<String,Object>> toSoyData(MatchingFile[] files, String repositoryUrl) {
+    List<Map<String, Object>> data = new ArrayList<>();
+    for (MatchingFile file: files) {
+      data.add(file.toSoyData(repositoryUrl));
+    }
+    return data;
+  }
+
+  private Map<String,Object> toSoyData(String repositoryUrl) {
+    Map<String, Object> data = new HashMap<>();
+    SoyListData branchMatches = new SoyListData();
+    for (String branchName: branches) {
+      String branchUrl = repositoryUrl + "+/" + branchName;
+      String fileUrl = branchUrl + "/" + fileName;
+      SoyMapData fileMatch = new SoyMapData(
+          "branchName", branchName,
+          "branchUrl", branchUrl,
+          "fileName", fileName,
+          "fileUrl", fileUrl,
+          "lines", MatchingLine.toSoyData(lines, fileUrl));
+      branchMatches.add(fileMatch);
+    }
+    data.put("matches", branchMatches);
+    return data;
+  }
+}
---
+++ gitiles-servlet/src/main/java/com/google/gitiles/search/MatchingLine.java
@@ -0,0 +1,63 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gitiles.search;
+
+import com.google.template.soy.data.SoyListData;
+import com.google.template.soy.data.SoyMapData;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class MatchingLine {
+
+  private int lineNumber;
+  private String line;
+  private Position[] matches;
+
+  static List<Map<String, Object>> toSoyData(MatchingLine[] lines, String fileUrl) {
+    List<Map<String, Object>> data = new ArrayList<>();
+    for (MatchingLine line : lines) {
+      data.add(line.toSoyData(fileUrl));
+    }
+    return data;
+  }
+
+  private Map<String, Object> toSoyData(String fileUrl) {
+    Map<String, Object> data = new HashMap<>();
+    data.put("lineNumber", Integer.toString(lineNumber));
+    data.put("lineUrl", fileUrl + "#" + lineNumber);
+
+    SoyListData highlighted = new SoyListData();
+    int pos = 0;
+    for (Position match : matches) {
+      if (match.start - pos > 0) {
+        addSpan(highlighted, pos, match.start, "plain");
+      }
+      addSpan(highlighted, match.start, match.end, "emphasized");
+      pos = match.end;
+    }
+    addSpan(highlighted, pos, line.length(), "pln");
+
+    data.put("line", highlighted);
+    return data;
+  }
+
+  private void addSpan(SoyListData data, int start, int end, String cssClass) {
+    if (end - start > 0) {
+      data.add(new SoyMapData("class", cssClass, "text", line.substring(start, end)));
+    }
+  }
+}
