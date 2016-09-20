fc4fefd63e7f411691d32863f620b1f43d31c9d1
Michael Ochmann <michael.ochmann@sap.com>
2016-09-20 13:20:21 +0200

    WIP: Code Search UI
    This patch aims at utilizing a zoekt server [1] as backend for
    a code search in gitiles. A new view is added, /+search, that
    displays the result of a code search in a given branch of a
    repository for which the zoekt server can provide index data.
    
    zoekt needs the name of a repository and at least one branch name
    as input. Branch names are not easily available in gitiles, though.
    Actually, only the repository index page calculates them, but the
    other views treat branches just a refs, which are not usable as
    input for zoekt yet. Therefore, the search form is visible only
    on the repository index page, although for layout reasons it had
    to be added to Common.soy. There is no possibility to select
    multiple (or all) branches for a search query yet in the UI to
    keep things simple.
    
    The search form submits the query and branch to the search as:
    
       /<repo>/+search?branch=<branch>&query=<query>
    
    The search view connects to the zoekt server, which has been
    configured in gitiles.conf, section [codeSearch], for example:
    
    [codeSearch]
      zoektServer = http://zoekt.example.org:6070
    
    TODOs:
      - support searching multiple branches ("all branches" option in
        the dropdown list)
      - make the search available on views, where we can determine a
        branch from the view context
      - extract the HTTP communication code and imrprobe error handling
      - css
    
    [1] https://github.com/google/zoekt
    
    Also-By: Saša Živkov <sasa.zivkov@sap.com>
    Change-Id: I1a01de9ab60baa817da5acf398ca2b5201ccd342

--- gitiles-servlet/src/main/java/com/google/gitiles/GitilesView.java
+++ gitiles-servlet/src/main/java/com/google/gitiles/GitilesView.java
@@ -67,7 +67,8 @@ public class GitilesView {
     ARCHIVE,
     BLAME,
     DOC,
-    ROOTED_DOC;
+    ROOTED_DOC,
+    SEARCH,
   }
 
   /** Exception thrown when building a view that is invalid. */
@@ -119,6 +120,7 @@ public class GitilesView {
         case ARCHIVE:
         case BLAME:
         case SHOW:
+        case SEARCH:
           path = other.path;
           //$FALL-THROUGH$
         case REVISION:
@@ -203,6 +205,7 @@ public class GitilesView {
         case REVISION:
         case ROOTED_DOC:
         case SHOW:
+        case SEARCH:
         default:
           this.revision = checkNotNull(revision);
           return this;
@@ -258,6 +261,7 @@ public class GitilesView {
         case DESCRIBE:
         case REFS:
         case LOG:
+        case SEARCH:
         case DOC:
         case ROOTED_DOC:
           break;
@@ -355,6 +359,9 @@ public class GitilesView {
         case ROOTED_DOC:
           checkRootedDoc();
           break;
+        case SEARCH:
+          checkSearch();
+          break;
       }
       return new GitilesView(
           type,
@@ -390,6 +397,10 @@ public class GitilesView {
       checkHostIndex();
     }
 
+    private void checkQuery() {
+      checkView(params != null && params.get("query") != null, "missing query string on %s view", type);
+    }
+
     private void checkRefs() {
       checkRepositoryIndex();
     }
@@ -409,6 +420,10 @@ public class GitilesView {
 
     private void checkLog() {
       checkRepositoryIndex();
+    }
+
+    private void checkSearch() {
+      checkQuery();
     }
 
     private void checkPath() {
@@ -466,6 +481,10 @@ public class GitilesView {
 
   public static Builder log() {
     return new Builder(Type.LOG);
+  }
+
+  public static Builder search() {
+    return new Builder(Type.SEARCH);
   }
 
   public static Builder archive() {
@@ -681,6 +700,16 @@ public class GitilesView {
           if (oldRevision != Revision.NULL) {
             url.append(oldRevision.getName()).append("..");
           }
+          url.append(revision.getName());
+          if (path != null) {
+            url.append('/').append(path);
+          }
+        }
+        break;
+      case SEARCH:
+        url.append(repositoryName).append("/+search");
+        if (revision != Revision.NULL) {
+          url.append('/');
           url.append(revision.getName());
           if (path != null) {
             url.append('/').append(path);
---
+++ gitiles-servlet/src/main/java/com/google/gitiles/search/SearchServlet.java
@@ -0,0 +1,122 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gitiles.search;
+
+import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+
+import com.google.common.base.CharMatcher;
+import com.google.gitiles.BaseServlet;
+import com.google.gitiles.GitilesAccess;
+import com.google.gitiles.GitilesView;
+import com.google.gitiles.LogServlet;
+import com.google.gitiles.Renderer;
+import com.google.gitiles.RepositoryDescription;
+import com.google.gitiles.ViewFilter;
+import com.google.gson.JsonSyntaxException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.http.Consts;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.eclipse.jgit.lib.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** Serves an HTML page with code-search results */
+public class SearchServlet extends BaseServlet {
+  private static final long serialVersionUID = 1L;
+  private static final Logger log = LoggerFactory.getLogger(LogServlet.class);
+
+  private final String searchEndpoint;
+
+  public SearchServlet(GitilesAccess.Factory accessFactory, Renderer renderer,
+      Config cfg) {
+    super(renderer, accessFactory);
+    String zoektServer = cfg.getString("codeSearch", null, "zoektServer");
+    searchEndpoint = zoektServer != null
+        ? CharMatcher.is('/').trimTrailingFrom(zoektServer).concat("/api/search")
+        : null;
+  }
+
+  @Override
+  protected void doGetHtml(HttpServletRequest req, HttpServletResponse res)
+      throws IOException {
+    if (searchEndpoint == null) {
+      res.setStatus(SC_NOT_FOUND);
+      return;
+    }
+    GitilesView view = ViewFilter.getView(req);
+
+    // TODO use  view.getParameters() instead
+    String query = req.getParameter("query");
+    String branches = req.getParameter("branches");
+
+    Restriction restriction = new Restriction(view.getRepositoryName(), branches);
+    Search search = new Search(query, restriction);
+
+    SearchResult searchResult = null;
+    try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+      HttpPost post = new HttpPost(searchEndpoint);
+      StringEntity reqEntity = new StringEntity(search.toJson(),
+          ContentType.create("application/json", Consts.UTF_8));
+      post.setEntity(reqEntity);
+      try (CloseableHttpResponse response = httpclient.execute(post)) {
+        if (response.getStatusLine().getStatusCode() != SC_OK) {
+          log.error("Unexpected response: " + response.getStatusLine());
+        }
+        HttpEntity respEntity = response.getEntity();
+        if (respEntity != null) {
+          String respContent = EntityUtils.toString(respEntity);
+          try {
+            searchResult = SearchResult.valueOf(respContent);
+          } catch (JsonSyntaxException e) {
+            log.error("Response content was: '" + respContent + "'");
+          }
+        }
+      }
+    }
+
+    Map<String, Object> data = new HashMap<>();
+    data.put("title", "Results for " + query);
+    data.put("query", query);
+
+    if (searchResult == null || searchResult.isEmpty()) {
+      renderHtml(req, res, "gitiles.emptyCodeSearchResult", data);
+      return;
+    }
+
+    String repositoryUrl = getRepositoryUrl(req, view);
+    data.put("repositoryUrl", repositoryUrl);
+    data.put("searchResult", searchResult.toSoyData(repositoryUrl));
+    renderHtml(req, res, "gitiles.codeSearchResult", data);
+  }
+
+  private String getRepositoryUrl(HttpServletRequest req, GitilesView view)
+      throws IOException {
+    RepositoryDescription repoDescr = getAccess(req).getRepositoryDescription();
+    return GitilesView.repositoryIndex().copyFrom(view)
+        .setRepositoryName(repoDescr.name).toUrl();
+  }
+}
