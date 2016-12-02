c34b34ee89514c81e62d1b958b1807289459a835
Andrew Bonventre <andybons@chromium.org>
2016-12-01 20:18:37 -0800

    Cache URLs containing SHA hashes for one day
    + The content of these urls will never change, and when navigating
      through a tree or traversing blame history, the cost of each request
      adds up.
    
    Change-Id: I3af9782036aa238dcae7b543935d16c46212dd61

--- gitiles-servlet/src/main/java/com/google/gitiles/BaseServlet.java
+++ gitiles-servlet/src/main/java/com/google/gitiles/BaseServlet.java
@@ -231,7 +231,7 @@ public abstract class BaseServlet extends HttpServlet {
       HttpServletRequest req, HttpServletResponse res, Map<String, ?> soyData) throws IOException {
     res.setContentType(FormatType.HTML.getMimeType());
     res.setCharacterEncoding(UTF_8.name());
-    setCacheHeaders(res);
+    setCacheHeaders(req, res);
 
     Map<String, Object> allData = getData(req);
 
@@ -323,7 +323,7 @@ public abstract class BaseServlet extends HttpServlet {
       throws IOException {
     res.setStatus(statusCode);
     setApiHeaders(req, res, TEXT);
-    setCacheHeaders(res);
+    setCacheHeaders(req, res);
     try (Writer out = newWriter(req, res)) {
       out.write(message);
     }
@@ -338,7 +338,25 @@ public abstract class BaseServlet extends HttpServlet {
     return access;
   }
 
-  protected void setCacheHeaders(HttpServletResponse res) {
+  protected void setCacheHeaders(HttpServletRequest req, HttpServletResponse res) {
+    String pragma = req.getHeader(HttpHeaders.PRAGMA);
+    if (!Strings.isNullOrEmpty(pragma) && pragma.equals("no-cache")) {
+      setNotCacheable(res);
+      return;
+    }
+    String cacheControl = req.getHeader(HttpHeaders.CACHE_CONTROL);
+    if (!Strings.isNullOrEmpty(cacheControl) && cacheControl.equals("no-cache")) {
+      setNotCacheable(res);
+      return;
+    }
+
+    GitilesView view = ViewFilter.getView(req);
+    Revision rev = view.getRevision();
+    if (rev.nameIsId()) {
+      res.setHeader(HttpHeaders.CACHE_CONTROL, "private, max-age=86400");  // Cache for 1 day.
+      return;
+    }
+
     setNotCacheable(res);
   }
 
@@ -366,7 +384,7 @@ public abstract class BaseServlet extends HttpServlet {
     } else {
       res.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
     }
-    setCacheHeaders(res);
+    setCacheHeaders(req, res);
   }
 
   protected void setApiHeaders(HttpServletRequest req, HttpServletResponse res, FormatType type)
@@ -374,10 +392,11 @@ public abstract class BaseServlet extends HttpServlet {
     setApiHeaders(req, res, type.getMimeType());
   }
 
-  protected void setDownloadHeaders(HttpServletResponse res, String filename, String contentType) {
+  protected void setDownloadHeaders(HttpServletRequest req, HttpServletResponse res,
+      String filename, String contentType) {
     res.setContentType(contentType);
     res.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename);
-    setCacheHeaders(res);
+    setCacheHeaders(req, res);
   }
 
   protected static Writer newWriter(OutputStream os, HttpServletResponse res) throws IOException {
