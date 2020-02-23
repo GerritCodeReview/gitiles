c57f8c947c0c7a94647f94459705cac6ca6cabdd
David Ostrovsky <david@ostrovsky.org>
2020-02-23 21:05:58 +0100

    Highlight changes within changed lines
    Using https://github.com/google/diff-match-patch.
    
    Author comment:
    
    I consider that to be a crude hack though, ideally JGit's would extend
    its diff interface to offer something more fine-grained than line-based.
    IIRC what I do right now is to use JGit's EditList to build back hunks
    of RawTexts (in a non line-oriented fashion), then throw Neil Fraser's
    diff-match-patch at the substrings to get the fine-grained list of
    changes. Would make more sense to integrate that in JGit than in gitiles
    IMHO.
    
    Requested by Eike Rathke on Wed, 17 Oct 2018 14:05:27 +0200.
    https://lists.freedesktop.org/archives/libreoffice/2018-October/081216.html
    
    Depends-On: https://gerrit-review.googlesource.com/c/zuul/jobs/+/328089
    Change-Id: I0bfaf6b68c90dda07dea687ebd00e81620e7b7b3

--- java/com/google/gitiles/HtmlDiffFormatter.java
+++ java/com/google/gitiles/HtmlDiffFormatter.java
@@ -16,18 +16,28 @@ package com.google.gitiles;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static name.fraser.neil.plaintext.diff_match_patch.Operation;
+import static org.eclipse.jgit.diff.Edit.Type.DELETE;
+import static org.eclipse.jgit.diff.Edit.Type.INSERT;
+import static org.eclipse.jgit.diff.Edit.Type.REPLACE;
+import static org.eclipse.jgit.lib.Constants.encodeASCII;
 import static org.eclipse.jgit.util.QuotedString.GIT_PATH;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
+import name.fraser.neil.plaintext.diff_match_patch;
 import org.apache.commons.text.StringEscapeUtils;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffEntry.ChangeType;
 import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.diff.Edit;
+import org.eclipse.jgit.diff.EditList;
 import org.eclipse.jgit.diff.RawText;
 import org.eclipse.jgit.patch.FileHeader;
 import org.eclipse.jgit.patch.FileHeader.PatchType;
@@ -46,6 +56,8 @@ final class HtmlDiffFormatter extends DiffFormatter {
   private static final byte[] LINE_DELETE_BEGIN = "<span class=\"Diff-delete\">".getBytes(UTF_8);
   private static final byte[] LINE_CHANGE_BEGIN = "<span class=\"Diff-change\">".getBytes(UTF_8);
   private static final byte[] LINE_END = "</span>\n".getBytes(UTF_8);
+  private static final byte[] MARK_BEGIN = "<span class=\"Diff-mark\">".getBytes(UTF_8);
+  private static final byte[] MARK_END = "</span>".getBytes(UTF_8);
 
   private final Renderer renderer;
   private final GitilesView view;
@@ -80,6 +92,155 @@ final class HtmlDiffFormatter extends DiffFormatter {
       format(hdr.toEditList(), a, b);
       getOutputStream().write(DIFF_END);
     }
+  }
+
+  /* adapted from org.eclipse.jgit.diff.DiffFormatter */
+  private int context = 3;
+  private static final byte[] noNewLine =
+      encodeASCII("\\ No newline at end of file\n"); // $NON-NLS-1$
+
+  private static boolean isEndOfLineMissing(final RawText text, final int line) {
+    return line == text.size() && text.isMissingNewlineAtEnd();
+  }
+
+  private int findCombinedEnd(final List<Edit> edits, final int i) {
+    int end = i + 1;
+    while (end < edits.size() && (combineA(edits, end) || combineB(edits, end))) end++;
+    return end - 1;
+  }
+
+  private boolean combineA(final List<Edit> e, final int i) {
+    return e.get(i).getBeginA() - e.get(i - 1).getEndA() <= 2 * context;
+  }
+
+  private boolean combineB(final List<Edit> e, final int i) {
+    return e.get(i).getBeginB() - e.get(i - 1).getEndB() <= 2 * context;
+  }
+
+  @Override
+  public void format(final EditList edits, final RawText a, final RawText b) throws IOException {
+    for (int curIdx = 0; curIdx < edits.size(); ) {
+      Edit curEdit = edits.get(curIdx);
+      final int endIdx = findCombinedEnd(edits, curIdx);
+      final Edit endEdit = edits.get(endIdx);
+
+      int aCur = Math.max(0, curEdit.getBeginA() - context);
+      int bCur = Math.max(0, curEdit.getBeginB() - context);
+      final int aEnd = Math.min(a.size(), endEdit.getEndA() + context);
+      final int bEnd = Math.min(b.size(), endEdit.getEndB() + context);
+
+      writeHunkHeader(aCur, aEnd, bCur, bEnd);
+
+      while (true) {
+        for (; aCur < curEdit.getBeginA(); aCur++, bCur++)
+          writeEditLine(LINE_CHANGE_BEGIN, a.getString(aCur));
+
+        if (curEdit.getType() == DELETE) {
+          /* sequence A has removed the region */
+          for (; aCur < curEdit.getEndA(); aCur++)
+            writeEditLine(LINE_DELETE_BEGIN, a.getString(aCur));
+          if (aCur == aEnd && isEndOfLineMissing(a, aEnd)) getOutputStream().write(noNewLine);
+        } else if (curEdit.getType() == INSERT) {
+          /* sequence B has inserted the region */
+          for (; bCur < curEdit.getEndB(); bCur++)
+            writeEditLine(LINE_INSERT_BEGIN, b.getString(bCur));
+          if (bCur == bEnd && isEndOfLineMissing(b, bEnd)) getOutputStream().write(noNewLine);
+        } else if (curEdit.getType() == REPLACE) {
+          /* sequence B has replaced the region with different content */
+          diff_match_patch dmp = new diff_match_patch();
+          dmp.Diff_Timeout = .050f;
+          LinkedList<diff_match_patch.Diff> diffs =
+              dmp.diff_main(
+                  a.getString(aCur, curEdit.getEndA(), true),
+                  b.getString(bCur, curEdit.getEndB(), true),
+                  false);
+          dmp.diff_cleanupSemantic(diffs);
+
+          /* write deletions */
+          writeChanges(diffs, false);
+          aCur = curEdit.getEndA();
+          if (aCur == aEnd && isEndOfLineMissing(a, aEnd)) getOutputStream().write(noNewLine);
+
+          /* write insertions */
+          writeChanges(diffs, true);
+          bCur = curEdit.getEndB();
+          if (bCur == bEnd && isEndOfLineMissing(b, bEnd)) getOutputStream().write(noNewLine);
+        }
+
+        if (curIdx++ < endIdx) curEdit = edits.get(curIdx);
+        else {
+          /* was the last edit in this hunk */
+          for (aCur = curEdit.getEndA(); aCur < aEnd; aCur++, bCur++)
+            writeEditLine(LINE_CHANGE_BEGIN, a.getString(aCur));
+          if (curEdit.getEndA() < aEnd && isEndOfLineMissing(a, aEnd))
+            getOutputStream().write(noNewLine);
+          if (curEdit.getEndB() < bEnd && isEndOfLineMissing(b, bEnd))
+            getOutputStream().write(noNewLine);
+          break;
+        }
+      }
+    }
+  }
+
+  /* Escape a string to HTML and write it to the output stream. */
+  private void writeHtml(final String str) throws IOException {
+    getOutputStream().write(StringEscapeUtils.escapeHtml4(str).getBytes(UTF_8));
+  }
+
+  /* Write an edit line in the given context. */
+  private void writeEditLine(final byte[] ctxLine, final String str) throws IOException {
+    getOutputStream().write(ctxLine);
+    writeHtml(str);
+    getOutputStream().write(LINE_END);
+  }
+
+  /* Write changes (for type == REPLACE). */
+  private void writeChanges(final LinkedList<diff_match_patch.Diff> diffs, final boolean insert)
+      throws IOException {
+    /* A changed line.  We delay flushing changes to the end of each
+     * line as we don't mark changes which cover a full line. */
+    LinkedList<Change> changedLine = new LinkedList();
+
+    for (diff_match_patch.Diff diff : diffs) {
+      boolean mark;
+      if (diff.operation == Operation.EQUAL) mark = false;
+      else if ((diff.operation == Operation.INSERT) == insert) mark = true;
+      else continue;
+
+      int i = 0;
+      do {
+        /* find next linefeed character */
+        final int j = diff.text.indexOf('\n', i);
+        if (j < 0)
+          /* the remaining of this diff is on the same line */
+          changedLine.add(new Change(diff.text.substring(i), mark));
+        else {
+          /* there is linefeed at index j: append the relevant chunk and flush */
+          if (i < j) changedLine.add(new Change(diff.text.substring(i, j), mark));
+          flushChangedLine(changedLine, insert);
+        }
+        i = j + 1;
+      } while (i > 0 && i < diff.text.length());
+    }
+    /* flush last line */
+    flushChangedLine(changedLine, insert);
+  }
+
+  /* Flush an insertion/deletion change line. */
+  private void flushChangedLine(LinkedList<Change> changes, final boolean insert)
+      throws IOException {
+    getOutputStream().write(insert ? LINE_INSERT_BEGIN : LINE_DELETE_BEGIN);
+    ListIterator<Change> iter = changes.listIterator();
+    while (iter.hasNext()) {
+      Change c = iter.next();
+      /* mark that change unless it covers the whole line (singleton change) */
+      final boolean mark = c.mark ? (iter.previousIndex() > 0 || iter.hasNext()) : false;
+      if (mark) getOutputStream().write(MARK_BEGIN);
+      writeHtml(c.text);
+      if (mark) getOutputStream().write(MARK_END);
+    }
+    getOutputStream().write(LINE_END);
+    changes.clear(); /* delete all elements in the list */
   }
 
   private void renderHeader(String header) throws IOException {
@@ -134,27 +295,14 @@ final class HtmlDiffFormatter extends DiffFormatter {
     super.writeHunkHeader(aStartLine, aEndLine, bStartLine, bEndLine);
     getOutputStream().write(HUNK_END);
   }
+}
 
-  @Override
-  protected void writeLine(char prefix, RawText text, int cur) throws IOException {
-    // Manually render each line, rather than invoke a Soy template. This method
-    // can be called thousands of times in a single request. Avoid unnecessary
-    // overheads by formatting as-is.
-    OutputStream out = getOutputStream();
-    switch (prefix) {
-      case '+':
-        out.write(LINE_INSERT_BEGIN);
-        break;
-      case '-':
-        out.write(LINE_DELETE_BEGIN);
-        break;
-      case ' ':
-      default:
-        out.write(LINE_CHANGE_BEGIN);
-        break;
-    }
-    out.write(prefix);
-    out.write(StringEscapeUtils.escapeHtml4(text.getString(cur)).getBytes(UTF_8));
-    out.write(LINE_END);
+final class Change {
+  protected final String text;
+  protected final boolean mark;
+
+  protected Change(final String text, final boolean mark) {
+    this.text = text;
+    this.mark = mark;
   }
 }
