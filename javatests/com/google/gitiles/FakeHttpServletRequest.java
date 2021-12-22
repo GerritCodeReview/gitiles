85a4c07a4a0b0c97da17e3e6bb80b4fef11cb617
Gerrit Code Review <no-reply@gerritcodereview.com>
2021-12-22 13:35:06 -0800

    Switch to javax.servlet:3.1.0
    Implement new required methods for Servlet 3.1 in test classes:
      Servlet 3.1 introduces several new abstract methods on interfaces or
      classes that existed in servlet 2.5. Provide a mimimal, throwing
      implementation that largely allows servlet 2.5-written classes to
      compile and run using servlet 3.1.
    
    PiperOrigin-RevId: 417877794
    Change-Id: I9fe856fab678ede1b58dd7c0731bc5ed5927ed49

--- javatests/com/google/gitiles/FakeHttpServletRequest.java
+++ javatests/com/google/gitiles/FakeHttpServletRequest.java
@@ -25,6 +25,7 @@ import com.google.common.collect.LinkedListMultimap;
 import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Maps;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.security.Principal;
@@ -34,11 +35,20 @@ import java.util.Enumeration;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import javax.servlet.AsyncContext;
+import javax.servlet.DispatcherType;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpUpgradeHandler;
+import javax.servlet.http.Part;
 import org.eclipse.jgit.http.server.ServletUtils;
 import org.eclipse.jgit.internal.storage.dfs.DfsRepository;
 
@@ -101,6 +111,11 @@ public class FakeHttpServletRequest implements HttpServletRequest {
   }
 
   @Override
+  public long getContentLengthLong() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
   public String getContentType() {
     return null;
   }
@@ -123,6 +138,42 @@ public class FakeHttpServletRequest implements HttpServletRequest {
   @Override
   public int getLocalPort() {
     return port;
+  }
+
+  @Override
+  public ServletContext getServletContext() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public AsyncContext startAsync() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse)
+      {
+        throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean isAsyncStarted() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean isAsyncSupported() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public AsyncContext getAsyncContext() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public DispatcherType getDispatcherType() {
+    throw new UnsupportedOperationException();
   }
 
   @Override
@@ -353,6 +404,11 @@ public class FakeHttpServletRequest implements HttpServletRequest {
   }
 
   @Override
+  public String changeSessionId() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
   public HttpSession getSession(boolean create) {
     throw new UnsupportedOperationException();
   }
@@ -376,6 +432,37 @@ public class FakeHttpServletRequest implements HttpServletRequest {
   @Deprecated
   public boolean isRequestedSessionIdFromUrl() {
     throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean authenticate(HttpServletResponse response) throws IOException, ServletException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void login(String username, String password) throws ServletException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void logout() throws ServletException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Collection<Part> getParts() throws IOException, ServletException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Part getPart(String name) throws IOException, ServletException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public <T extends HttpUpgradeHandler> T upgrade(Class<T> httpUpgradeHandlerClass)
+      throws IOException, ServletException {
+        throw new UnsupportedOperationException();
   }
 
   @Override
