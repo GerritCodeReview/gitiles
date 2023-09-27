bc5802e4c97dd0a676feea7729905771237470f2
Réda Housni Alaoui <reda-alaoui@hey.com>
2023-09-27 19:34:14 +0200

    Add Mermaid support to Markdown documents
    Change-Id: I9db9002f61e361a2f331e8cb2cf0dca067478932

---
+++ java/com/google/gitiles/doc/Script.java
@@ -0,0 +1,13 @@
+package com.google.gitiles.doc;
+
+import org.commonmark.node.CustomBlock;
+
+/**
+ * @author Réda Housni Alaoui
+ */
+public class Script extends CustomBlock {
+
+	String type;
+	String literal;
+
+}
