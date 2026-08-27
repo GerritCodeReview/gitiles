9be41ec722e7a468bf3e0e78988168c9a1f52b39
George Engelbrecht <engeg@google.com>
2026-08-27 14:28:19 +0000

    Add canonical cross-browser favicon package
    Introduce a complete, brand-neutral Git favicon set based on the
    Git coral-orange squircle with white branching commit nodes.
    
    Includes:
    - favicon.svg: Lossless scalable vector for modern browsers
    - favicon.ico: Multi-resolution binary (16x16, 32x32, 48x48)
    - favicon-32x32.png & favicon-16x16.png: High-DPI and standard PNGs
    - apple-touch-icon.png: 180x180 for iOS/macOS Safari bookmarks
    - android-chrome-192x192.png & android-chrome-512x512.png: PWA assets
    - site.webmanifest: Modern web app manifest definition
    
    Updates Renderer.java and HTML head templates in Common.soy and Doc.soy
    to declare the canonical favicon links. Adds comprehensive servlet unit
    tests in FaviconTest.
    
    Change-Id: Ia4e17c7a17da2813a767a0d4f000afc5c6ccc284

---
+++ javatests/com/google/gitiles/FaviconTest.java
@@ -0,0 +1,84 @@
+// Copyright (C) 2026 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gitiles;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for favicon links and static asset mapping in Gitiles HTML pages. */
+@RunWith(JUnit4.class)
+public class FaviconTest extends ServletTest {
+
+  private void assertFaviconElementsPresent(String html) {
+    assertThat(html).contains("rel=\"icon\" type=\"image/svg+xml\"");
+    assertThat(html).contains("rel=\"icon\" type=\"image/png\" sizes=\"32x32\"");
+    assertThat(html).contains("rel=\"icon\" type=\"image/png\" sizes=\"16x16\"");
+    assertThat(html).contains("rel=\"apple-touch-icon\" sizes=\"180x180\"");
+    assertThat(html).contains("rel=\"manifest\"");
+    assertThat(html).contains("favicon.svg");
+    assertThat(html).contains("favicon-32x32.png");
+    assertThat(html).contains("favicon-16x16.png");
+    assertThat(html).contains("apple-touch-icon.png");
+    assertThat(html).contains("site.webmanifest");
+  }
+
+  @Test
+  public void staticUrlGlobalsContainsAllFaviconEntries() {
+    assertThat(Renderer.STATIC_URL_GLOBALS)
+        .containsEntry("gitiles.FAVICON_SVG_URL", "favicon.svg");
+    assertThat(Renderer.STATIC_URL_GLOBALS)
+        .containsEntry("gitiles.FAVICON_32_URL", "favicon-32x32.png");
+    assertThat(Renderer.STATIC_URL_GLOBALS)
+        .containsEntry("gitiles.FAVICON_16_URL", "favicon-16x16.png");
+    assertThat(Renderer.STATIC_URL_GLOBALS)
+        .containsEntry("gitiles.APPLE_TOUCH_ICON_URL", "apple-touch-icon.png");
+    assertThat(Renderer.STATIC_URL_GLOBALS)
+        .containsEntry("gitiles.SITE_MANIFEST_URL", "site.webmanifest");
+  }
+
+  @Test
+  public void hostIndexIncludesFavicons() throws Exception {
+    String html = buildHtml("/", false);
+    assertFaviconElementsPresent(html);
+  }
+
+  @Test
+  public void repositoryIndexIncludesFavicons() throws Exception {
+    String html = buildHtml("/repo/", false);
+    assertFaviconElementsPresent(html);
+  }
+
+  @Test
+  public void docPageIncludesFavicons() throws Exception {
+    repo.branch("master")
+        .commit()
+        .add("README.md", "# Test Document\n\nDoc content.")
+        .create();
+
+    String html = buildHtml("/repo/+doc/master/README.md", false);
+    assertFaviconElementsPresent(html);
+  }
+
+  @Test
+  public void commitDetailIncludesFavicons() throws Exception {
+    RevCommit commit = repo.branch("master").commit().add("foo", "contents").create();
+    String html = buildHtml("/repo/+/" + commit.name(), false);
+    assertFaviconElementsPresent(html);
+  }
+}
